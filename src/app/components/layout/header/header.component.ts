9638fd8da9d79358398513f8a9a8aed9cef2a5df
Leidi De La Puente <leididelapuente3@gmail.com>
2025-07-02 18:03:33 -0500

    feat: create HeaderComponent for layout management with font and theme toggles

---
+++ src/app/components/layout/header/header.component.ts
@@ -0,0 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FontToggleComponent } from '../fonts-toggle/font-toggle.component';
+import { ThemeToggleComponent } from "../theme-toggle/theme-toggle.component";
+
+@Component({
+  selector: 'app-header',
+  imports: [NgOptimizedImage, FontToggleComponent, ThemeToggleComponent],
+  templateUrl: './header.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeaderComponent { }
