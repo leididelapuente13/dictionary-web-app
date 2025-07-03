2f0df38f2e7deb0c0aba9126ee592fa5bb77e9cc
Leidi De La Puente <leididelapuente3@gmail.com>
2025-07-02 19:18:09 -0500

    update: update main component to include dictionary search bar in layout

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -3,11 +3,12 @@ import { NgClass } from '@angular/common';
 
 import { HeaderComponent } from "../../components/layout/header/header.component";
 import { FontFamilyStateService } from '../../services/state/FontFamilyState.service';
+import { SearchBarComponent } from "../../components/search-bar/search-bar.component";
 
 
 @Component({
   selector: 'app-main',
-  imports: [NgClass, HeaderComponent],
+  imports: [NgClass, HeaderComponent, SearchBarComponent],
   templateUrl: './main.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
