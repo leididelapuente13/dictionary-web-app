d0d86ccde5078c3815fe3703ae3628ca603decab
Leidi De La Puente <leididelapuente3@gmail.com>
2025-07-05 01:06:15 -0500

    feat: enhance main component with dictionary entry loading and error handling

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,22 +1,37 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, resource, signal } from '@angular/core';
 import { NgClass } from '@angular/common';
 
 import { HeaderComponent } from "../../components/layout/header/header.component";
-import { FontFamilyStateService } from '../../services/state/FontFamilyState.service';
+import { FontFamilyStateService, DictionaryService } from '../../services';
 import { SearchBarComponent } from "../../components/search-bar/search-bar.component";
 import { EntryArticleComponent } from "../../components/dictionary/entry-article/entry-article.component";
+import { NotFoundComponent } from "../../components/feedback/not-found/not-found.component";
+import { firstValueFrom } from 'rxjs';
+import { LoaderComponent } from "../../components/feedback/loader/loader.component";
 
 
 @Component({
   selector: 'app-main',
-  imports: [NgClass, HeaderComponent, SearchBarComponent, EntryArticleComponent],
+  imports: [NgClass, HeaderComponent, SearchBarComponent, EntryArticleComponent, NotFoundComponent, LoaderComponent],
   templateUrl: './main.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainComponent {
-  fontFamilyState = inject(FontFamilyStateService);
 
-  get currentFontFamily (){
+  fontFamilyState = inject(FontFamilyStateService);
+  dictionaryService = inject(DictionaryService);
+
+  get currentFontFamily() {
     return this.fontFamilyState.currentFont()
   }
+
+  wordToSearch = signal<string>('');
+
+  dictionaryEntryResource = resource({
+    request: ()=>({word: this.wordToSearch()}),
+    loader: ({request})=>{
+      return firstValueFrom(this.dictionaryService.getDictionaryEntry(request.word));
+    }
+  })
+
 }
