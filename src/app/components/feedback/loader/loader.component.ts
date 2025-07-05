3663f5e04a1f519ef5e46604f8a187483094d9a3
Leidi De La Puente <leididelapuente3@gmail.com>
2025-07-05 01:05:53 -0500

    feat: add LoaderComponent with loading indicator template

---
+++ src/app/components/feedback/loader/loader.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  imports: [],
+  templateUrl: './loader.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoaderComponent {
+  
+}
