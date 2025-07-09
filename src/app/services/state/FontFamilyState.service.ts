d8b914a38b3b5ee68c85e59a3c6c353cb9788ee4
Leidi De La Puente <leididelapuente3@gmail.com>
2025-07-08 22:44:44 -0500

    update: add localstorage to keep the chosen font

--- src/app/services/state/FontFamilyState.service.ts
+++ src/app/services/state/FontFamilyState.service.ts
@@ -6,9 +6,25 @@ import { FontFamilyTypes } from '../../interfaces/fonts.interface';
 })
 export class FontFamilyStateService {
 
-  currentFont = signal<FontFamilyTypes>('serif')
+  FONT_KEY = 'dictionary-app-theme';
+  currentFont = signal<FontFamilyTypes>(this.getFont());
 
-  changeFont(font: FontFamilyTypes) {
-    this.currentFont.set(font);
+  private storeCurrentFontInLocalStorage(font: FontFamilyTypes) {
+    localStorage.setItem(this.FONT_KEY, font);
   }
+
+  private getFontInLocalStorage(): FontFamilyTypes | undefined {
+    const fontInLocalStorage = localStorage.getItem(this.FONT_KEY);
+    return fontInLocalStorage as FontFamilyTypes ?? undefined;
+  }
+
+  private getFont(): FontFamilyTypes {
+    return this.getFontInLocalStorage() ?? 'serif';
+  }
+
+  public changeFont(font: FontFamilyTypes) {
+    this.currentFont.set(font);
+    this.storeCurrentFontInLocalStorage(this.currentFont());
+  }
+
 }
